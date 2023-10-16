de106e370945d5b53cb397476fb6da4f612a75d4
Giova262 <jogiovah@gmail.com>
2023-10-16 17:31:13 -0300

    rebuild

--- dist/spa/assets/i18n.88f80eb6.js
+++ dist/spa/assets/i18n.88f80eb6.js
@@ -1,4 +1,4 @@
-import{d as it,h as jt,e as pn,i as In,o as Tn,a as bn,r as Te,c as be,w as qe,F as Bt,g as Jt,b as On,f as hn,T as Cn,j as An}from"./index.f4a80ed7.js";/*!
+import{d as it,h as jt,e as pn,i as In,o as Tn,a as bn,r as Te,c as be,w as qe,F as Bt,g as Jt,b as On,f as hn,T as Cn,j as An}from"./index.f56387e2.js";/*!
   * shared v9.5.0
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
