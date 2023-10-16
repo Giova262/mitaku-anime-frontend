2a261ca8ab76ee6b455460b4752e70b665ea95e9
Giova262 <jogiovah@gmail.com>
2023-10-16 13:40:46 -0300

    refactoring

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,11 +4,11 @@
       <div class="flex q-py-sm">
         <span class="q-pr-sm">Telfefono/Whatsapp: +54 911 2235 1904</span>
 
-        <q-avatar size="20px" class="q-mx-md">
+        <q-avatar size="20px" class="q-mr-md">
           <img src="~assets/whatsapp.png" />
         </q-avatar>
 
-        <span class="q-px-sm q-mr-md">mitaku.anime@gmail.com</span>
+        <span class="q-px-sm q-mr-sm">mitaku.anime@gmail.com</span>
 
         <q-avatar size="20px" class="">
           <img src="~assets/gmail.png" />
@@ -16,10 +16,10 @@
 
         <q-space />
 
-        <q-avatar size="20px" class="">
+        <q-avatar size="20px" class="q-mx-sm">
           <img src="~assets/facebook.png" />
         </q-avatar>
-        <q-avatar size="20px">
+        <q-avatar size="20px" class="q-mx-sm">
           <img src="~assets/instagram.png" />
         </q-avatar>
       </div>
@@ -59,21 +59,18 @@
           </div>
 
           <div class="col">
-            <h5>Contacts</h5>
+            <div class="text-bold" style="font-size: 18px">Contacts</div>
             <dl class="contact-list">
-              <dt>Direccion:</dt>
-              <dd>798 South Park Avenue, New York, USA</dd>
+              <dt>Direccion: 798 South Park Avenue, New York, USA</dt>
             </dl>
             <dl class="contact-list">
-              <dt>Email:</dt>
-              <dd><a href="mailto:#">compagny@email.com</a></dd>
+              <dt>Email: compagny@email.com</dt>
             </dl>
             <dl class="contact-list">
-              <dt>Telefonos:</dt>
-              <dd>
-                <a href="tel:#">1.555.652.258</a> <span>o </span>
+              <dt>
+                Telefonos: <a href="tel:#">1.555.652.258</a> <span>o </span>
                 <a href="tel:#">1.55.652.268</a>
-              </dd>
+              </dt>
             </dl>
           </div>
         </div>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,42 +7,26 @@
         infinite
         v-model="slide"
         height="400px"
-        :autoplay="12000"
+        :autoplay="1000"
       >
         <q-carousel-slide
-          name="first"
-          img-src="https://cdn.quasar.dev/img/mountains.jpg"
+          v-for="(slide, i) in itemsCarousel" :key="i + 'slide'"
+          :name="slide.name"
+          :img-src="slide.url"
         >
           <div class="absolute-bottom custom-caption">
-            <div class="text-h2">First stop</div>
-            <div class="text-subtitle1">Mountains</div>
+            <div class="text-h2">{{ slide.title }}</div>
+            <div class="text-subtitle1">{{ slide.subtitle }}</div>
           </div>
         </q-carousel-slide>
-        <q-carousel-slide
-          name="second"
-          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
-        >
-          <div class="absolute-bottom custom-caption">
-            <div class="text-h2">Second stop</div>
-            <div class="text-subtitle1">Famous City</div>
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide
-          name="third"
-          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
-        >
-          <div class="absolute-bottom custom-caption">
-            <div class="text-h2">Third stop</div>
-            <div class="text-subtitle1">Famous Bridge</div>
-          </div>
-        </q-carousel-slide>
+
       </q-carousel>
     </div>
 
     <div
       ref="scrollTargetRef"
       class="q-px-md container q-my-md"
-      style="height: 800px; overflow: auto; width: auto;"
+      style="height: 800px; overflow: auto; width: auto"
     >
       <q-infinite-scroll
         @load="onLoadRef"
@@ -50,8 +34,12 @@
         :scroll-target="scrollTargetRef"
         class="q-mt-md"
       >
-        <div v-for="(item, index) in itemsRef" :key="index" class="caption row q-mt-md">
-          <div class="q-pa-md items-start q-gutter-md col ">
+        <div
+          v-for="(item, index) in itemsRef"
+          :key="index"
+          class="caption row q-mt-md"
+        >
+          <div class="q-pa-md items-start q-gutter-md col">
             <q-card class="my-card" flat bordered>
               <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
 
@@ -90,7 +78,7 @@
               </q-slide-transition>
             </q-card>
           </div>
-          <div class="q-pa-md items-start q-gutter-md col ">
+          <div class="q-pa-md items-start q-gutter-md col">
             <q-card class="my-card" flat bordered>
               <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
 
@@ -123,13 +111,13 @@
                 <div v-show="expanded">
                   <q-separator />
                   <q-card-section class="text-subtitle2">
-                    {{ lorem }}
+
                   </q-card-section>
                 </div>
               </q-slide-transition>
             </q-card>
           </div>
-          <div class="q-pa-md items-start q-gutter-md col ">
+          <div class="q-pa-md items-start q-gutter-md col">
             <q-card class="my-card" flat bordered>
               <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
 
@@ -162,7 +150,7 @@
                 <div v-show="expanded">
                   <q-separator />
                   <q-card-section class="text-subtitle2">
-                    {{ lorem }}
+
                   </q-card-section>
                 </div>
               </q-slide-transition>
@@ -190,93 +178,52 @@ export default defineComponent({
   setup() {
     const itemsRef = ref([{}, {}, {}, {}, {}, {}, {}]);
     const itemsId = ref([{}, {}, {}, {}, {}, {}, {}]);
-    const scrollTargetRef = ref(null);
-    const items = ref([
+    const itemsCarousel = ref([
       {
         id: 1,
-        name: "Ruddy Jedrzej",
-        email: "rjedrzej0@discuz.net",
-        letter: "R",
+        name: 1,
+        url: "https://cdn.quasar.dev/img/mountains.jpg",
+        title: "First stop 1",
+        subtitle: "Mountains 1",
       },
       {
         id: 2,
-        name: "Mallorie Alessandrini",
-        email: "malessandrini1@marketwatch.com",
-        letter: "M",
+        name: 2,
+        url: "https://cdn.quasar.dev/img/parallax1.jpg",
+        title: "First stop 3",
+        subtitle: "Mountains 3",
       },
       {
         id: 3,
-        name: "Elisabetta Wicklen",
-        email: "ewicklen2@microsoft.com",
-        letter: "E",
-      },
-      {
-        id: 4,
-        name: "Seka Fawdrey",
-        email: "sfawdrey3@wired.com",
-        letter: "S",
-      },
-      {
-        id: 3,
-        name: "Elisabetta Wicklen",
-        email: "ewicklen2@microsoft.com",
-        letter: "E",
-      },
-      {
-        id: 4,
-        name: "Seka Fawdrey",
-        email: "sfawdrey3@wired.com",
-        letter: "S",
-      },
-      {
-        id: 4,
-        name: "Seka Fawdrey",
-        email: "sfawdrey3@wired.com",
-        letter: "S",
-      },
-      {
-        id: 3,
-        name: "Elisabetta Wicklen",
-        email: "ewicklen2@microsoft.com",
-        letter: "E",
-      },
-      {
-        id: 4,
-        name: "Seka Fawdrey",
-        email: "sfawdrey3@wired.com",
-        letter: "S",
+        name: 3,
+        url: "https://cdn.quasar.dev/img/parallax2.jpg",
+        title: "First stop 4",
+        subtitle: "Mountains 4",
       },
     ]);
+    const scrollTargetRef = ref(null);
+    const slide = ref(1);
 
+    function onLoadRef(index, done) {
+      setTimeout(() => {
+        itemsRef.value.push({}, {}, {}, {}, {}, {}, {});
+        done();
+      }, 2000);
+    }
+    function onLoadId(index, done) {
+      setTimeout(() => {
+        itemsId.value.push({}, {}, {}, {}, {}, {}, {});
+        done();
+      }, 2000);
+    }
     return {
+      itemsCarousel,
       itemsRef,
       itemsId,
       scrollTargetRef,
-
-      onLoadRef(index, done) {
-        setTimeout(() => {
-          itemsRef.value.push({}, {}, {}, {}, {}, {}, {});
-          done();
-        }, 2000);
-      },
-
-      onLoadId(index, done) {
-        setTimeout(() => {
-          itemsId.value.push({}, {}, {}, {}, {}, {}, {});
-          done();
-        }, 2000);
-      },
-      items,
-      slide: ref("first"),
-      onLoad(index, done) {
-        console.log("onloading");
-        /*setTimeout(() => {
-        console.log('alhgo')
-
-          items.value.splice(0, 0, {}, {}, {}, {}, {}, {}, {})
-          done()
-        }, 2000)*/
-      },
+      slide,
+      onLoadRef,
+      onLoadId,
     };
   },
 });
